e0761ad2826673041a49e129d12f6493fe452dd8
tamaratosic98 <tosict98@gmail.com>
2021-03-08 18:00:51 +0100

    Napravljene klase Artikal, KucnaHemija, KancelarijskiMaterijal i Knjiga sa get i set metodama.

---
+++ src/magacin/KucnaHemija.java
@@ -0,0 +1,16 @@
+package magacin;
+
+import java.util.Date;
+
+public class KucnaHemija extends Artikal{
+	private Date rokTrajanja;
+
+	public Date getRokTrajanja() {
+		return rokTrajanja;
+	}
+
+	public void setRokTrajanja(Date rokTrajanja) {
+		this.rokTrajanja = rokTrajanja;
+	}
+	
+}
