d50d2818b6251b5403143ead8fefaa877b063694
tamaratosic98 <tosict98@gmail.com>
2021-03-08 18:30:02 +0100

    Implementirana klasa Magacin na drugaciji nacin.

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -1,24 +1,46 @@
 package magacin;
 
+import java.util.LinkedList;
+
 import interfejs.MagacinInterface;
 
 public class Magacin implements MagacinInterface {
-
+	private LinkedList<Artikal> artikli=new LinkedList<Artikal>();
 	@Override
 	public void dodajArtikal(Artikal artikal) {
 		// TODO Auto-generated method stub
+		if(artikal!=null && !artikli.contains(artikal)) {
+			artikli.add(artikal);
+		}
+		else if(artikal!=null) {
+			for(Artikal a:artikli) {
+				if(a.equals(artikal)) {
+					a.setKolicina(a.getKolicina()+artikal.getKolicina());
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal!=null) {
+			for(Artikal a:artikli) {
+				if(a.equals(artikal)) {
+					a.setKolicina(a.getKolicina()-artikal.getKolicina());
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for(Artikal a:artikli) {
+			if(a.getSifra()==sifra) {
+				return a;
+			}
+		}
 		return null;
 	}
 
