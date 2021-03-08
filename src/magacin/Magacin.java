f2a1488848ff118871e0734d2c7ac5becc3bdeaa
tamaratosic98 <tosict98@gmail.com>
2021-03-08 18:18:01 +0100

    Kreirana klasa Magacin koja implementira MagacinInterface.

---
+++ src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import interfejs.MagacinInterface;
+
+public class Magacin implements MagacinInterface {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
